815b20866918c5f2221b02caa39b9bb7f7e03fd8
SergioGeek <sergio.it.geek@gmail.com>
2018-02-20 17:25:00 +0100

    la primera función lo lee todo

--- module1.py
+++ module1.py
@@ -1,47 +1,70 @@
 
 from bs4 import BeautifulSoup
 
-
-with open("/home/anonymous/Desktop/coleccionESuja2018/es_26142.html") as ls:
-
-	soup = BeautifulSoup(ls, "html.parser")
+from os import listdir
 
 
 
 
-
-#Get Title
-print soup.title.string
+	
 
 
-#Get Date
-date = soup.find("div", attrs = {"class" : "field-item odd"})
-print date.find("span").string
+def filter( arch ):
 
-#Get Body
-body = soup.find_all("p")
-print body[1].string
+	with open("/home/anonymous/Desktop/coleccionESuja2018/" + arch) as ls:
 
-#Get Tags
-topics = soup.find_all("a", attrs = {"rel" : "tag"})
+		soup = BeautifulSoup(ls, "html.parser")
 
-for i in topics:
-	print i.string
 
-#Get Rute
-rute = soup.find("div", attrs = {"class" : "breadcrumb"})
 
-rute.find_all("a")
+	
 
-#for i in rute:
-	#print i.string
+	#Get Title
+	print soup.title.string
 
-inta = 0
-#Get Author
-author = soup.find("div", attrs = {"class" : "submitted"})
-user = author.string.split()
 
-print user[2]
+	#Get Date
+	date = soup.find("div", attrs = {"class" : "field-item odd"})
+	print date.find("span").string
+
+	#Get Body
+	body = soup.find_all("p")
+	print body[1].string
+
+	#Get Tags
+	topics = soup.find_all("a", attrs = {"rel" : "tag"})
+
+	for i in topics:
+		print i.string
+
+	#Get Rute
+	rute = soup.find("div", attrs = {"class" : "terms terms-inline"})
+
+	if rute:
+		print rute.find("a").string
+	else:
+		print "home"
+		
+
+	#Get Author
+	author = soup.find("div", attrs = {"class" : "submitted"})
+	user = author.string.split()
+
+	print user[2]
+
+	ls.close()
+	
+
+
+
+
+
+
+for arch in listdir("/home/anonymous/Desktop/coleccionESuja2018/"):
+
+	print "!!!!!!!!!!!!!!!!!!!!!!!!" + arch + "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
+
+	filter( arch )
 
 
 print "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
